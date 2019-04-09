4c09151322cd203c7a89762761a0d9725e12fb6b
Jamal <jamal@hku.hk>
2019-04-09 22:46:46 +0800

    testing fix

--- dataloader.py
+++ dataloader.py
@@ -24,6 +24,7 @@ class DataLoader(object):
             phone_files = sorted(glob(os.path.join(self.config.dataset_dir, self.phone, self.mode, self.phone, "*")))
             dslr_files = sorted(glob(os.path.join(self.config.dataset_dir, self.phone, self.mode, "canon/*")))
         elif self.mode == "test_data/full_size_test_images":
+            print("test files loading")
             phone_files = sorted(glob(os.path.join(self.config.dataset_dir, self.phone, self.mode, "*")))
         else:
             phone_files = sorted(glob(os.path.join(self.config.dataset_dir, self.phone, self.mode, self.phone, "*")))[
