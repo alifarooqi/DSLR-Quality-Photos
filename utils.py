5a198892c1117948e09690470ac1b62a2525d702
Jamal <jamal@hku.hk>
2019-04-10 22:08:41 +0800

    load full size files for testing

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def load_files(files, res, test_mode):
         loaded = [scipy.misc.imread(filename, mode="RGB") for filename
                   in files]
     else:
-        loaded = [resize_img(scipy.misc.imread(filename, mode="RGB"), (1500, 1500)) for filename
+        loaded = [scipy.misc.imread(filename, mode="RGB") for filename
                   in files]
     return loaded
 
