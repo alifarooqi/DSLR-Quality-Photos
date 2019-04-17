15cabc1d63234e15484a0c6881332e690f0875a4
Jamal <jamal@hku.hk>
2019-04-17 15:16:12 +0800

    run custom images: debugging

--- test.py
+++ test.py
@@ -60,6 +60,7 @@ if __name__ == '__main__':
         inputs, rets, gts = model.test()
         counter2 = 0
         for input, ret, gt in zip(inputs, rets, gts):
+            print(counter2)
             imsave(os.path.join(config.testing_dir, str(counter2)+"_input.jpg"), postprocess(input))
             imsave(os.path.join(config.testing_dir, str(counter2)+"_output.jpg"), postprocess(ret))
             counter2 += 1
