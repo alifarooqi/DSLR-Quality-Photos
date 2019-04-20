a544b5cf9c7a81bb26baf53a46e5bf0d88cf0c98
Jamal <jamal@hku.hk>
2019-04-20 20:19:03 +0800

    eval code

--- eval.py
+++ eval.py
@@ -17,9 +17,9 @@ config.test_dir = os.path.join(config.test_dir, config.phone_model, "patches/*")
 files = sorted(glob(config.test_dir))
 num_samples = int(len(files) / 3)
 
-gts = np.zeros((1, 100, 100, 3))
-inputs = np.zeros((1, 100, 100, 3))
-outputs = np.zeros((1, 100, 100, 3))
+gts = np.zeros((1, 100, 100, 3), dtype=np.float32)
+inputs = np.zeros((1, 100, 100, 3), dtype=np.float32)
+outputs = np.zeros((1, 100, 100, 3), dtype=np.float32)
 for i in range(1):
     print(files[i * 3], files[i * 3 + 1], files[i * 3 + 2])
     gts[i] = imread(files[i * 3], mode="RGB")
