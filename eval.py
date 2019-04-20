eea364e6f1079c4427ab17a054386a038a434a6c
Jamal <jamal@hku.hk>
2019-04-20 19:04:08 +0800

    eval code

---
+++ eval.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+from glob import glob
+from scipy.misc import imread
+import numpy as np
+from utils import gaussian_blur
+
+parser = argparse.ArgumentParser(description="testing options")
+
+parser.add_argument("phone_model", type=str, help="phone model to test")
+parser.add_argument("--test_dir", type=str, default="D:/FYPdenoising/dslr/test")
+
+config = parser.parse_args()
+
+config.test_dir = os.path.join(config.test_dir, config.phone_model, "patches/*")
+
+files = sorted(glob(config.test_dir))
+num_samples = int(len(files) / 3)
+
+for i in range(1):
+    gt_img = imread(files[i * 3], mode="RGB")
+    input_img = imread(files[i * 3 + 1], mode="RGB")
+    output_img = imread(files[i * 3 + 2], mode="RGB")
+    loss = np.mean(np.square(gaussian_blur(gt_img) - gaussian_blur(output_img)))
+
